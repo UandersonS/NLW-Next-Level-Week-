6fc648d9baaf45e82004f258f9cdccdae8885968
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-10-16 14:09:25 -0300

    Add files via upload

---
+++ NLW#3 - Discovery (3-4)/src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        
+        id: 1,
+        lat: "-27.220002",
+        lng: "-49.6491757",
+        name = "Lar das Meninas",
+        description = "Presta assistencia a criancas de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images:[
+            "https://images.unsplash.com/photo-1582167410766-a90eea4787ae?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1573924436756-128cf1f6387a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+        opening_hours: "Horario de visitas das 18h ate 8h",
+        open_on_weekends:"1",
+        
+    },
+    {
+        id: 2,
+        lat: "-27.220002",
+        lng: "-49.6591757",
+        name = "Lar dos Meninos",
+        description = "Presta assistencia a criancas de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images:[
+            "https://images.unsplash.com/photo-1582167410766-a90eea4787ae?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1573924436756-128cf1f6387a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+        opening_hours: "Horario de visitas das 18h ate 8h",
+        open_on_weekends:"0",
+        
+    }
+]
